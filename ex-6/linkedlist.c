f480c3ed44c9c4b4318e4c3d7583a9169ade5e82
Akul Abrol <146575710+AkulAbrol@users.noreply.github.com>
2023-10-05 21:47:43 +0530

    Add files via upload

---
+++ ex-6/linkedlist.c
@@ -0,0 +1,36 @@
+#include<linkedlist.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void insertionatbeginning(){
+     struct node *ptr;   
+     int item;
+     ptr = (struct node *)malloc(sizeof(struct node));  
+     if(ptr == NULL)  
+   {  
+       printf("\nOVERFLOW");  
+   }  
+   else  
+   {  
+    printf("\nEnter Item value");  
+    scanf("%d",&item);  
+      
+   if(head==NULL)  
+   {  
+       ptr->next = NULL;  
+       ptr->prev=NULL;  
+       ptr->data=item;  
+       head=ptr;  
+   }  
+   else   
+   {  
+       ptr->data=item;  
+       ptr->prev=NULL;  
+       ptr->next = head;  
+       head->prev=ptr;  
+       head=ptr;  
+   }  
+   printf("\nNode inserted\n");  
+}  
+}  
+    
---
+++ ex-6/linkedlist.h
@@ -0,0 +1,7 @@
+typedef struct node{
+    int data;
+    struct node *prev,*next;
+}node;
+
+struct node *head;
+void insertionatbeginning();
